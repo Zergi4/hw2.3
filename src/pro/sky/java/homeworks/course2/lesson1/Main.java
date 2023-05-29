f490c6792b6d7b12e1a0226322775dda1a74a8f3
Serg <ksyssau@ya.ru>
2023-05-29 19:46:13 +0300

    1 задача

---
+++ src/pro/sky/java/homeworks/course2/lesson1/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+package pro.sky.java.homeworks.course2.lesson1;
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
