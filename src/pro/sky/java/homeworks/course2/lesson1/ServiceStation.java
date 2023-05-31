10bd9e17b7c44757b93ddbe3b1854c21db5ced0f
Serg <ksyssau@ya.ru>
2023-05-31 19:43:54 +0300

    2

--- src/pro/sky/java/homeworks/course2/lesson1/ServiceStation.java
+++ src/pro/sky/java/homeworks/course2/lesson1/ServiceStation.java
@@ -9,17 +9,8 @@ public class ServiceStation {
             }
             car.checkEngine();
         } else if (truck != null) {
-            System.out.println("Обслуживаем " + truck.modelName);
-            for (int i = 0; i < truck.wheelsCount; i++) {
-                truck.updateTyre();
-            }
-            truck.checkEngine();
-            truck.checkTrailer();
-        } else if (bicycle != null) {
-            System.out.println("Обслуживаем " + bicycle.modelName);
-            for (int i = 0; i < bicycle.wheelsCount; i++) {
-                bicycle.updateTyre();
-            }
+
+        }
         }
     }
 }
--- src/pro/sky/java/homeworks/course2/lesson1/Truck.java
+++ src/pro/sky/java/homeworks/course2/lesson1/Truck.java
@@ -2,8 +2,29 @@ package pro.sky.java.homeworks.course2.lesson1;
 
 import pro.sky.java.homeworks.course2.lesson1.WheelsTransport;
 
-public class Truck extends WheelsTransport {
+public class Truck extends WheelsTransport implements Repair{
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void check() {
+        System.out.println("Обслуживаем " + this.getModelName());
+        for (int i = 0; i < this.getWheelsCount(); i++) {
+           updateTyre();
+        }
+        checkEngine();
+        checkTrailer();
+    }
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
     }
 }
